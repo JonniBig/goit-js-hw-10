78f06af7661f9a42c3dfdd3aaa49489457c33bc0
JonniBig <bigun.i88@gmail.com>
2023-08-14 12:34:16 +0300

    mark1

---
+++ src/index.js
@@ -0,0 +1,86 @@
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
+import { fetchBreeds, fetchCatByBreed } from './js/cats-api';
+import { Report } from 'notiflix/build/notiflix-report-aio';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://api.thecatapi.com/v1';
+axios.defaults.headers.common['x-api-key'] =
+  'live_32QuEtSUa4B1mBDYZcQ5jGz3DaJl5LlsKHCv42KCYUzXQiHOoAVCQBpMPmosGqYV';
+
+const breedSelect = document.querySelector('.breed-select');
+const loader = document.querySelector('.loader');
+
+breedSelect.addEventListener('change', onSelect);
+const catInfo = document.querySelector('.cat-info');
+
+// create function to choose exact breed of cat
+function onSelect(event) {
+  catInfo.innerHTML = '';
+  loader.style.display = 'block';
+  fetchCatByBreed(event.currentTarget.value)
+    .then(resp => {
+      catInfo.innerHTML = createMarkupCatCard(resp.data);
+    })
+    .catch(function (error) {
+      onError();
+
+      console.log(error);
+    })
+    .finally(function () {
+      loader.style.display = 'none'; // always executed
+    });
+}
+
+// setTimeout to show select when all list of breeds came
+setTimeout(() => {
+  breedSelect.style.display = 'block';
+}, 0);
+
+// create function to get list of all breeds
+fetchBreeds()
+  .then(response => {
+    const storedBreeds = response.data;
+    breedSelect.innerHTML = createMarkupSelect(storedBreeds);
+    slim();
+  })
+  .catch(function (error) {
+    onError();
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    loader.style.display = 'none';
+  });
+
+// create markup for breed list
+function createMarkupSelect(arr) {
+  return arr
+    .map(({ id, name }) => `<option value="${id}">${name}</option>`)
+    .join('');
+}
+
+// create murkup for one breed
+function createMarkupCatCard(arr) {
+  const url = arr[0].url;
+  const description = arr[0].breeds[0].description;
+  return `<img
+        src="${url}"
+        alt=""
+        width="480px"
+      />
+      <p>
+        ${description}
+      </p>`;
+}
+// create new beautiful select element
+function slim() {
+  new SlimSelect({
+    select: '#breed-select',
+  });
+}
+
+// function for error handling
+function onError() {
+  Report.failure('Okay', 'Oops! Something went wrong! Try reloading the page!');
+}
